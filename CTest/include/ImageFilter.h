37fd94ef09fb006bd110647dc1ad36f7f2137106
brinalesser <49353127+brinalesser@users.noreply.github.com>
2021-08-05 14:26:40 -0700

    Added comments

--- CTest/include/ImageFilter.h
+++ CTest/include/ImageFilter.h
@@ -1,3 +1,11 @@
+/**
+ * Header file for c++ vision program that processes each frame 
+ * of a video pixel by pixel
+ * 
+ * @author Sabrina Lesser (Sabrina.Lesser@rfpco.com)
+ * @date 8/5/2021
+ **/
+
 #ifndef IMAGE_FILTER
 #define IMAGE_FILTER
 
@@ -5,6 +13,12 @@
 #include <iostream>
 #include <string>
 
+/**
+ * Process a single frame pixel by pixel
+ * 
+ * @param im the original frame as a cv::Mat object
+ * @return the processed frame as a cv::Mat object
+ **/
 cv::Mat process_image(cv::Mat im);
 
 #endif
