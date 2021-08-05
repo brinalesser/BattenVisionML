c89c3e81f2de49084e71178014a1885f93802afd
brinalesser <49353127+brinalesser@users.noreply.github.com>
2021-08-05 14:32:40 -0700

    Added comments

--- CTest/include/PLCCommTest.h
+++ CTest/include/PLCCommTest.h
@@ -1,3 +1,10 @@
+/**
+ * Header file for the c++ program thhat tests reading from and writing to PLC tags using the libplctag library
+ * 
+ * @author Sabrina Lesser (Sabrina.Lesser@rfpco.com)
+ * @date 8/5/2021
+ **/
+
 #ifndef PLC_COMM_TEST
 #define PLC_COMM_TEST
 
@@ -6,11 +13,14 @@
 #include <unistd.h>
 #include <strings.h>
 #include <stdint.h>
+
+//PLC communications library
 #include <libplctag.h>
 
 #define snprintf_platform snprintf
 #define sscanf_platform sscanf
 	
+//paramaters for creating plc tag handles
 #define REQUIRED_VERSION 2,1,0
 #define LED_TAG_PATH "protocol=ab_eip&gateway=10.1.8.15&path=1,2&debug=1&cpu=controllogix&name=OUTPUT_LEDS"
 #define SW_TAG_PATH "protocol=ab_eip&gateway=10.1.8.15&path=1,2&debug=1&cpu=controllogix&name=INPUT_SWITCHES"
@@ -22,7 +32,6 @@ extern "C" {
 
 extern int util_sleep_ms(int ms);
 extern int64_t util_time_ms(void);
-
 
 #ifdef __cplusplus
 }
