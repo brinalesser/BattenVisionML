2aa5003301fdf7d7f06191bdd0be6649cd4f8efd
brinalesser <49353127+brinalesser@users.noreply.github.com>
2021-08-11 11:52:10 -0700

    Added comments

--- ImageFilter.py
+++ ImageFilter.py
@@ -1,8 +1,22 @@
+'''
+This program was an attempt to process frames pixel by pixel.
+-> THIS IS TOO SLOW IF USING PYTHON so I attempted to use cython,
+but it's easier and more efficient to just use C or C++
 
+@author Sabrina Lesser (Sabrina.Lesser@rfpco.com)
+@date last modified: 8/11/21
+'''
 import cv2 as cv
 import numpy as np
 import cython
 
+'''
+cython function to process a frame pixel by pixel
+
+@param thresh the grayscale threshold
+@param im a grayscale image
+@return the filtered image 
+'''
 @cython.boundscheck(False)
 cpdef unsigned char[:, :] process_image(int thresh, unsigned char [:, :] im):
     cdef int x, y, w, h
@@ -20,19 +34,25 @@ cpdef unsigned char[:, :] process_image(int thresh, unsigned char [:, :] im):
                 '''
     return im
 
-cap = cv.VideoCapture(0) #0 is for raspberry pi usb camera
+'''
+main
+'''
+cap = cv.VideoCapture(0) #0 is for raspberry pi usb camera (replace with filename for video)
 
 if(cap.isOpened() == False):
     print("failed to open camera")
 
 while(cap.isOpened()):
+    #get and show original frame
     ret, frame = cap.read()
     cv.imshow("Original", frame)
     if(ret):
+        #convert frame to grayscale and display
         frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        cv.imshow("Original",frame)
+        cv.imshow("Grayscale",frame)
+        #process frame and display
         new_frame = process_image(frame)
-        cv.imshow("New",new_frame)
+        cv.imshow("Processed",new_frame)
     else:
         print("failed to read frame")
         break
